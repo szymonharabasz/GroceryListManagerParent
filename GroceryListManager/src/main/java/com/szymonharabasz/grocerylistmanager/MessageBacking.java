95cdaa6b32062afc9837e069067810adb199dd04
Szymon Harabasz <szymon.harabasz@gmail.com>
2021-12-30 01:23:53 +0100

    MessageBacking refactored to use a resource bundle and not repeat the code.

--- GroceryListManager/src/main/java/com/szymonharabasz/grocerylistmanager/MessageBacking.java
+++ GroceryListManager/src/main/java/com/szymonharabasz/grocerylistmanager/MessageBacking.java
@@ -29,35 +29,44 @@ public class MessageBacking {
     }
 
     public void load() {
-        if (Objects.equals(type, "email-sent")) {
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, ResourceBundle.getBundle("com.szymonharabasz.grocerylistmanager.texts").getString("email-sent.summary"),
-                            "An e-mail has been sent to the address you provided in the registration. " +
-                                    "Check your mailbox and click the confirmation link to activate your account."));
-            title = "Confirmation e-mail sent";
-            header = "Confirmation e-mail sent";
-        } else if (Objects.equals(type, "password-changed")) {
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Success",
-                            "Your password has been successfuly changed."));
-            title = "Password changed";
-            header = "Password changed";
-        } else if (Objects.equals(type, "password-reset-requested")) {
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Check your e-mail",
-                            "An e-mail has been sent to the address you provided in the registration. " +
-                                    "Check your mailbox and click the reset link to change your password."));
-            title = "Password reset requested";
-            header = "Password reset requested";
-        }
-        else if (Objects.equals(type, "wrong-token")) {
-            facesContext.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Problem with the link",
-                            "A wrong token has been provided. Check the link you have received in the e-mail." +
-                                    "The link could also have been used already."));
-            title = "Wrong token";
-            header = "Wrong token";
-        }
+        FacesMessage.Severity severity = Objects.equals(type, "wrong-token") ?
+                FacesMessage.SEVERITY_ERROR :
+                FacesMessage.SEVERITY_INFO;
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("com.szymonharabasz.grocerylistmanager.texts");
+        facesContext.addMessage(null,
+                new FacesMessage(severity, resourceBundle.getString(type + ".summary"),
+                        resourceBundle.getString(type + ".detail")));
+        title = resourceBundle.getString(type + ".title");
+        header = resourceBundle.getString(type + ".header");
+//        if (Objects.equals(type, "email-sent")) {
+//            facesContext.addMessage(null,
+//                    new FacesMessage(FacesMessage.SEVERITY_INFO, resourceBundle.getString("email-sent.summary"),
+//                            "An e-mail has been sent to the address you provided in the registration. " +
+//                                    "Check your mailbox and click the confirmation link to activate your account."));
+//            title = "Confirmation e-mail sent";
+//            header = "Confirmation e-mail sent";
+//        } else if (Objects.equals(type, "password-changed")) {
+//            facesContext.addMessage(null,
+//                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Success",
+//                            "Your password has been successfuly changed."));
+//            title = "Password changed";
+//            header = "Password changed";
+//        } else if (Objects.equals(type, "password-reset-requested")) {
+//            facesContext.addMessage(null,
+//                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Check your e-mail",
+//                            "An e-mail has been sent to the address you provided in the registration. " +
+//                                    "Check your mailbox and click the reset link to change your password."));
+//            title = "Password reset requested";
+//            header = "Password reset requested";
+//        }
+//        else if (Objects.equals(type, "wrong-token")) {
+//            facesContext.addMessage(null,
+//                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Problem with the link",
+//                            "A wrong token has been provided. Check the link you have received in the e-mail." +
+//                                    "The link could also have been used already."));
+//            title = "Wrong token";
+//            header = "Wrong token";
+//        }
     }
 
     public String getType() {
